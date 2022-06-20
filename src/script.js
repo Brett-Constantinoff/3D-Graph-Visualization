6d9359215ba989ca3e019ef848f48da019ac4ee5
Brett Constantinoff <constantinoffbrett@gmail.com>
2022-06-20 10:45:26 -0600

    adds new background image and OOP design * creates the app class * adds a new background image for the scene

--- src/script.js
+++ src/script.js
@@ -7,138 +7,156 @@ A basic Three.js app reuires 4 main things:
 */
 import './style.css'
 import * as THREE from 'three';
-import { OrbitContols, OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import * as dat from 'dat.gui';
 import Stats from 'stats.js';
 
-//setup stats
-const stats = new Stats();
-stats.showPanel(0);
-document.body.appendChild(stats.dom);
-
-const scene = new THREE.Scene();
-
-//create our red cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const materialProps = {
-    color: 'red',
-    wireframe: false
-};
-const material = new THREE.MeshPhongMaterial(materialProps);
-const mesh = new THREE.Mesh(geometry, material);
-mesh.translateX(-1.5);
-scene.add(mesh);
-
-//add a point light with a white color
-const light = new THREE.PointLight( 0x404040, 5, 100);
-light.position.set(0, 10, 10);
-scene.add(light);
-
-//setup our camera and move it back a bit
-const sizes = {
-    width: 800,
-    height: 600
-};
-const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height);
-scene.add(camera);
-
-//get our canvas for the renderer
-const canvas = document.querySelector('.webgl');
-const renderer = new THREE.WebGLRenderer(
+class App
+{
+    constructor()
     {
-        canvas: canvas,
-        antialias: true
+
     }
-);
-renderer.setSize(sizes.width, sizes.height);
-renderer.setClearColor('grey');
 
-//setup debgug GUI
-const gui = new dat.GUI();
-const cubeFolder = gui.addFolder("Cube(s)");
+    onStart()
+    {
+        //setup stats
+        this.stats = new Stats();
+        this.stats.showPanel(0);
+        document.body.appendChild(this.stats.dom);
 
-cubeFolder.add(materialProps, 'wireframe') 
-    .onChange((value) => material.wireframe = value);
+        this.scene = new THREE.Scene();
 
-const colors = {
-    cubeColor : material.color
-};
-cubeFolder.addColor(colors, 'cubeColor')
-    .onChange((value) => 
-        {
-            material.color.r = value.r / 255;
-            material.color.g = value.g / 255;
-            material.color.b = value.g / 255;
-        }
-    );
+        //create our red cube
+        const geometry = new THREE.BoxGeometry(1, 1, 1);
+        const materialProps = {
+            color: 'red',
+            wireframe: false
+        };
+        const material = new THREE.MeshPhongMaterial(materialProps);
+        this.mesh = new THREE.Mesh(geometry, material);
+        this.mesh.translateX(-1.5);
+        this.scene.add(this.mesh);
 
-const sizeFolder = cubeFolder.addFolder("Size");
+        //add a point light with a white color
+        const light = new THREE.PointLight( 0x404040, 5, 100);
+        light.position.set(0, 10, 10);
+        this.scene.add(light);
 
-const size = {
-    x: mesh.scale.x,
-    y: mesh.scale.y, 
-    z: mesh.scale.z
-};
-sizeFolder.add(size, 'x', 1.0, 15.0)
-    .onChange((value) => mesh.scale.x = value);
+        //setup our camera and move it back a bit
+        this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.scene.add(this.camera);
 
-sizeFolder.add(size, 'y', 1.0, 15.0)
-    .onChange((value) => mesh.scale.y = value);
+        //get our canvas for the renderer
+        const canvas = document.querySelector('.webgl');
 
-sizeFolder.add(size, 'z', 1.0, 15.0)
-    .onChange((value) => mesh.scale.z = value);
-
-//clone makes an exact copy and saves all the changes
-const mesh2 = mesh.clone();
-mesh2.translateX(3.0);
-
-const addSecondCube = {
-    add: false
-};
-gui.add(addSecondCube, 'add').name("Add Cube")
-    .onChange((value) => 
-        {
-            if(value)
+        this.renderer = new THREE.WebGLRenderer(
             {
-                scene.add(mesh2);
+                canvas: canvas,
+                antialias: true
             }
-            else
-            {
-                scene.remove(mesh2);
-            }
-        }
-    );
+        );
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.setClearColor('grey');
 
-const controls = new OrbitControls(camera, renderer.domElement);
-camera.translateZ(5.0);
-controls.update();
+        //setup debgug GUI
+        const gui = new dat.GUI();
+        const cubeFolder = gui.addFolder("Cube(s)");
+
+        cubeFolder.add(materialProps, 'wireframe') 
+            .onChange((value) => material.wireframe = value);
+
+        const colors = {
+            cubeColor : material.color
+        };
+        cubeFolder.addColor(colors, 'cubeColor')
+            .onChange((value) => 
+                {
+                    material.color.r = value.r / 255;
+                    material.color.g = value.g / 255;
+                    material.color.b = value.g / 255;
+                }
+            );
+
+        const sizeFolder = cubeFolder.addFolder("Size");
+
+        const size = {
+            x: this.mesh.scale.x,
+            y: this.mesh.scale.y, 
+            z: this.mesh.scale.z
+        };
+        sizeFolder.add(size, 'x', 1.0, 15.0)
+            .onChange((value) => this.mesh.scale.x = value);
+
+        sizeFolder.add(size, 'y', 1.0, 15.0)
+            .onChange((value) => this.mesh.scale.y = value);
+
+        sizeFolder.add(size, 'z', 1.0, 15.0)
+            .onChange((value) => this.mesh.scale.z = value);
+
+        //clone makes an exact copy and saves all the changes
+        const mesh2 = this.mesh.clone();
+        mesh2.translateX(3.0);
+
+        const addSecondCube = {
+            add: false
+        };
+        gui.add(addSecondCube, 'add').name("Add Cube")
+            .onChange((value) => 
+                {
+                    if(value)
+                    {
+                        this.scene.add(mesh2);
+                    }
+                    else
+                    {
+                        this.scene.remove(mesh2);
+                    }
+                }
+            );
+
+        //setup camera controls
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        this.camera.translateZ(5.0);
+        this.controls.update();
+
+        //set a background image
+        const image = document.getElementById('back.jpg');
+        const texture = new THREE.TextureLoader().load(
+            image.src
+        );
+
+        this.scene.background = texture;
+    }
+
+    onUpdate(dt)
+    {
+        this.stats.begin();
+    }
+
+    onRender()
+    {
+
+        this.stats.end();
+        this.renderer.render(this.scene, this.camera);
+    }
+}
 
 /*
 Main Render Loop
 */
+const app = new App();
+app.onStart();
+
 const clock = new THREE.Clock();
 function render()
 {
-    stats.begin();
-
     const dt = clock.getDelta();
+    app.onUpdate(dt);
 
-    /*
-    mesh.rotation.y += dt * 0.5;
-    mesh.rotation.x += dt * -0.5;
-    mesh.rotation.z += dt * 0.5;
 
-    if(addSecondCube.add)
-    {
-        mesh.rotation.y += dt * 0.5;
-        mesh2.rotation.x += dt * -0.5;
-        mesh2.rotation.z += dt * 0.5;
-    }
-    */
-
-    renderer.render(scene, camera);
-
-    stats.end();
+    app.onRender();
     requestAnimationFrame(render);
 }
 
