ba2ef424dc9a33ea6f32d9e2de34b5798ee1474b
Brett Constantinoff <constantinoffbrett@gmail.com>
2022-10-12 20:05:51 -0600

    use stack based dfs

--- src/Algorithms/depthFirst.js
+++ src/Algorithms/depthFirst.js
@@ -6,53 +6,51 @@
  */
 export function depthFirstSearch(nodePos, maze)
 {
+    let stack = [];
     // get current node object
     let node = maze.getNode(nodePos);
+    stack.push(node);
 
-    let n;
-    maze.adjList.forEach((value, key) => {
-        if (key.x === nodePos.x && key.y === nodePos.y && key.z === nodePos.z) 
-        {
-            n = value;
-        }
-    })
-
-    // if node exists
-    if (node)
+    while (stack.length > 0)
     {
-        if (node.type === "path")
-        {
-            node.material.color.set(0xD288A2);
-            node.material.opacity = 1.0;
-        }
-        // mark the current node as visited
-        node.visited = true;
-        // get a random neighbour and make it a path
-        let index = Math.floor(Math.random() * n.length);
-        if (index != 0)
-            index--;
-        let randomPath = maze.getNode(n[index]);
-        if (node.type != "end")
-        {
-            randomPath.type = "path";
-        }
-  
-        // perfrom dfs on each neighbour recursivly
-        n.forEach((neighbourPos) => {
-            let neighbour = maze.getNode(neighbourPos);
-            if (!neighbour.visited)
+        let currNode = stack.pop();
+        let currPos = currNode.mesh.position;
+
+        // get neighbors
+        let n = [];
+        maze.adjList.forEach((value, key) => {
+            if ((key.x == currPos.x) && (key.y == currPos.y) && (key.z == currPos.z))
             {
-                // again, this cant be in its own function for some reason
-                let n;
-                maze.adjList.forEach((value, key) => {
-                    if (key.x === neighbourPos.x && key.y === neighbourPos.y && key.z === neighbourPos.z) 
-                    {
-                        n = value;
-                    }
-                })
-                // recurse
-                depthFirstSearch(neighbourPos, maze);
+                // get a list of unvisited neighbors
+                value.forEach((val) => {
+                    if (!maze.getNode(val).visited)
+                        n.push(val);
+                });
             }
-        })
+        });
+
+        if (n.length > 0)
+        {
+            // push cell back to stack
+            stack.push(currNode);
+
+            //choose a random neighbor
+            let index = Math.floor(Math.random() * (n.length - 1));
+            let neighbor = maze.getNode(n[index]);
+            
+            // quit once end is found
+            if (neighbor.type === "end")
+                return
+            neighbor.visited = true;
+            if (neighbor.type != "start")
+            {
+                neighbor.material.color.set(0x66ff00);
+                neighbor.type = "path"; 
+                stack.push(neighbor);
+            }
+       
+        }
+        
     }
+  
 }
