4b12c6007f8b6d41dcde17f834e7c7e9fe98c65e
Brett Constantinoff <constantinoffbrett@gmail.com>
2022-11-27 11:09:39 -0700

    update dijkstra

--- src/Algorithms/dijkstra.js
+++ src/Algorithms/dijkstra.js
@@ -12,46 +12,26 @@ export function dijkstra(maze)
         let minNode = getMin(maze, seen);
         seen.add(minNode);
 
-        // for visualization
-        let node = 
-        {
-            mesh: minNode.getMesh(),
-            neighbours: []
-        };
-
         // for each neighbor of minimum node
         for (let i = 0; i < neighborInfo.length; i++)
         {
-            // for visualization
-            let neighbourObj = 
-            {
-                mesh: null,
-                weight: null
-            };
-
             // get neighbor node
             let neighbor = maze.getNode(neighborInfo[i].neighbour);
-          
-            // set the parent of the neighbor to the current node
-            neighbor.parent = minNode;
-
+            
             // get the weight of the current neighbor
             let weight = neighborInfo[i].weight;
-            
-            // for visualization
-            neighbourObj.mesh = neighbor.mesh;
-            neighbourObj.weight = weight;
-            node.neighbours.push(neighbourObj);
-            
             // update neighbor distance
             if (seen.has(neighbor) === false && neighbor.distance > minNode.distance + weight)
             {
                 neighbor.distance = minNode.distance + weight;
+
+                // set the parent of the neighbor to the current node
+                neighbor.parent = minNode;
             }
         }
-        // for visualization
-        maze.algVis.dijkstra.order.push(node);
     }
+
+    dijkstraShortesPath(maze.endNode, maze)
 }
 
 function getMin(maze, seen)
@@ -76,7 +56,7 @@ function dijkstraShortesPath(endNode, maze)
     console.log(endNode)
     // iterate through node parents from end
     let currNode = endNode;
-    /*
+    
     while (currNode.parent.type != "start")
     {
         maze.algVis.dijkstra.shortestPath.push(currNode);
@@ -87,5 +67,5 @@ function dijkstraShortesPath(endNode, maze)
     // reverse array after
     maze.algVis.dijkstra.shortestPath.reverse();
     console.log(maze.algVis.dijkstra.shortestPath)
-    */
+
 }
